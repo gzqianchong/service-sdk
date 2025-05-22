1d3efd2b5437aca83b11b9297359a42edd8312f1
qwf <>
2025-05-22 14:09:51 +0800

    first commit

---
+++ src/Cores/Features/Feature.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Cores\Feature;
+
+use App\Cores\Core;
+
+abstract class Feature extends Core
+{
+}
---
+++ src/Models/Model.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class Model extends \Illuminate\Database\Eloquent\Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Str::orderedUuid()->toString();
+        });
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    final public static function init()
+    {
+        return new static();
+    }
+}
